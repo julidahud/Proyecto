d5724e476c83055c226250d6506a0a29574cc449
Micaiaspe <peraltamicaias36@gmail.com>
2025-10-22 18:24:52 -0300

    Hice cambios en el js

--- script3.js
+++ script3.js
@@ -3,18 +3,4 @@ const nav = document.querySelector("nav");
 
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
-
-  if (nav.classList.contains("active")) {
-    menuBtn.textContent = "✕";
-  } else {
-    menuBtn.textContent = "☰";
-  }
-});
-
-const links = document.querySelectorAll("nav a");
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    nav.classList.remove("active");
-    menuBtn.textContent = "☰";
-  });
 });
